257a5cbf93f522d612cb2596eb26022cc3ab2018
PijusM <pijus.nesakysiu12@gmail.com>
2022-06-05 16:50:51 +0300

    changes

--- script.js
+++ script.js
@@ -31,6 +31,21 @@ $(window).scroll(function() {
     });
 });
 
+$(window).scroll(function() {
+    $('.website').each(function() {
+        var top_of_element = $(this).offset().top;
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
+            $(this).addClass('is-visible');
+        }
+        else if ((bottom_of_screen < top_of_element) && (top_of_screen > bottom_of_element) && $(this).hasClass('is-visible')) {
+            $(this).removeClass('is-visible');
+        }
+    });
+});
 document.body.className = 'hidden';
 window.addEventListener('load', function(){
     document.body.className = 'visible';
